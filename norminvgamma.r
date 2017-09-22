193bec2b06367c2cae6bc3c3a9ea32fa40f64bef
Carl Scheffler <carl.scheffler@gmail.com>
2017-09-22 16:01:42 +0200

    Created norminvgamma.r
    Function definitions for the normal-inverse-gamma distribution.

---
+++ norminvgamma.r
@@ -0,0 +1,18 @@
+# Function definitions for the normal-inverse-gamma distribution.
+# The parameters of the distribution (mu, lambda, alpha, beta) are as defined here:
+# https://en.wikipedia.org/wiki/Normal-inverse-gamma_distribution
+
+# NOTE: The functions below require the function definitions in invgamma.r
+
+dnorminvgamma <- function(x, sigma2, mu, lambda, alpha, beta) {
+    # The pdf of the normal-inverse-gamma distribution at x (mean) and sigma2 (variance).
+    return(dnorm(x, mu, lambda * sigma2) * dinvgamma(sigma2, alpha, beta));
+}
+
+rnorminvgamma <- function(n, mu, lambda, alpha, beta) {
+    # Draw n samples from the normal-inverse-gamma distribution.
+    # Returns a matrix where each column contains a (x, sigma2) sample.
+    sigma2 = rinvgamma(n, alpha, beta);  # Sample sigma^2 from the inverse-gamma
+    x = rnorm(n, mu, sqrt(sigma2 / lambda));  # Sample x from the normal
+    return(t(matrix(c(x, sigma2), nrow=n, ncol=2)));
+}
